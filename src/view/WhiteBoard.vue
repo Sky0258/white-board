bbcc0b459ce168b9a108576005797d1b28f413ee
skybiubiubiu <1823471207@qq.com>
2024-06-03 20:58:02 +0800

    贝塞尔曲线控制点未设置

--- src/view/WhiteBoard.vue
+++ src/view/WhiteBoard.vue
@@ -1,13 +1,56 @@
 <template>
-  <canvas ref="canvasRef" width="800" height="600"></canvas>
+  <div>
+    <canvas ref="canvasRef" width="800" height="600"></canvas>
+  </div>
 </template>
  
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted } from "vue";
 const canvasRef = ref(null);
+
 
 onMounted(() => {
   const canvas = canvasRef.value;
-  console.log(canvas); // 这里应该能够正确获取到 canvas 元素
+  const ctx = canvas.getContext("2d");
+
+  // 设置画板线条的大小，背景色等
+  ctx.lineWidth = 2;
+  ctx.strokeStyle = "black";
+  let flag = false;
+  ctx.fillStyle = "#e0e0e0";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  // 平滑所画的曲线
+  // // 开启抗锯齿
+  // ctx.imageSmoothingEnabled = true;
+  // // 设置线条样式
+  // ctx.lineJoin = 'round';
+
+
+  // 鼠标按下绘图
+  canvas.onmousedown = (e) => {
+    flag = true;
+    ctx.beginPath();
+    ctx.moveTo(e.offsetX, e.offsetY);
+  };
+  // 鼠标抬起停止绘图
+  canvas.onmouseup = (e) => {
+    ctx.closePath();
+    // 首尾封闭成图形并填色
+    // ctx.fillStyle = "red";
+    // ctx.fill();
+    ctx.quadraticCurveTo(200, 200, e.offsetX, e.offsetY);
+    flag = false;
+  };
+
+  // 鼠标移动绘图
+  canvas.onmousemove = (e) => {
+    if (flag) {
+      console.log(e);
+      // ctx.lineTo(e.offsetX, e.offsetY);
+      ctx.quadraticCurveTo(200,200, e.offsetX, e.offsetY);
+      ctx.stroke();
+    }
+  };
 });
 </script>
