5185d4c50a36f69da59e164a649a17d3f6b21de8
skybiubiubiu <1823471207@qq.com>
2024-06-04 17:45:38 +0800

    尺子可以吸附画线

--- src/view/WhiteBoard.vue
+++ src/view/WhiteBoard.vue
@@ -5,8 +5,11 @@
         {
           paintType = 1;
         }
-      ">画任意线</button>
-    <button @click="drawRuler(300, 300)">直尺</button><br />
+      "
+    >
+      画任意线
+    </button>
+    <button @click="handleShowRuler(300, 300)">直尺</button><br />
     <canvas ref="canvasRef" width="1200" height="700"></canvas>
   </div>
 </template>
@@ -19,6 +22,7 @@ let ctx = null;
 let flag = false;
 let paintType = ref(1);
 
+// 初始化 canvas
 function initCanvas() {
   canvas = canvasRef.value;
   ctx = canvas.getContext("2d");
@@ -27,12 +31,9 @@ function initCanvas() {
   ctx.strokeStyle = "black";
   ctx.fillStyle = "#e0e0e0";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-  // 平滑所画的曲线
-  // // 开启抗锯齿
-  // ctx.imageSmoothingEnabled = true;
-  // ctx.lineJoin = 'round';
 }
 
+// 1.任意线模块
 function handlePaintLineStart(e) {
   flag = true;
   ctx.beginPath();
@@ -51,6 +52,8 @@ function handlePaintLineEnd() {
   flag = false;
 }
 
+// 2.移动尺子模块
+let isShowRuler = ref(false);
 // 记录尺子左上角位置
 const rulerPosition = reactive({
   x: 300,
@@ -70,35 +73,65 @@ const distanceToRuler = reactive({
 
 const isDragging = ref(false);
 
+// 判断是否在尺子上
+function checkOnRuler(e) {
+  const { x, y } = rulerPosition;
+  if (
+    e.offsetX >= x &&
+    e.offsetX <= x + 400 &&
+    e.offsetY >= y &&
+    e.offsetY <= y + 70
+  ) {
+    distanceToRuler.distanceX = e.offsetX - x;
+    distanceToRuler.distanceY = e.offsetY - y;
+    isDragging.value = true;
+    paintType.value = 2;
+    return true;
+  } else {
+    paintType.value = 1;
+  }
+  return false;
+}
+
+// 显示尺子 
+function handleShowRuler(x, y) {
+  isShowRuler.value = !isShowRuler.value;
+  drawRuler(x, y);
+}
+
+// 画尺子
 function drawRuler(x, y) {
-  paintType.value = 2;
   // 把之前的尺子删掉
   ctx.fillStyle = "#e0e0e0";
-  ctx.fillRect(preRulerPosition.x - 10, preRulerPosition.y - 10, 420, 90);
+  ctx.fillRect(preRulerPosition.x - 1, preRulerPosition.y, 420, 90);
 
-  preRulerPosition.x = x;
-  preRulerPosition.y = y;
+  if (isShowRuler.value) {
+    preRulerPosition.x = x;
+    preRulerPosition.y = y;
 
-  ctx.fillStyle = "white";
-  ctx.fillRect(x, y, 400, 70);
+    ctx.fillStyle = "white";
+    ctx.fillRect(x, y, 400, 70);
 
-  // 绘制横向刻度
-  ctx.beginPath();
-  ctx.moveTo(x, y);
-  ctx.lineTo(x + 400, y);
-  ctx.stroke();
-
-  // 绘制竖向刻度
-  for (let i = 0; i <= 400; i += 20) {
+    // 绘制横向刻度
+    ctx.strokeStyle = "#D4D2CF";
     ctx.beginPath();
-    ctx.moveTo(x + i, y);
-    let yLine = i % 100 == 0 ? 23 : 10;
-    ctx.lineTo(x + i, y + yLine);
+    ctx.moveTo(x, y + 1);
+    ctx.lineTo(x + 400, y + 1);
     ctx.stroke();
+
+    // 绘制竖向刻度
+    for (let i = 0; i <= 400; i += 20) {
+      ctx.beginPath();
+      ctx.moveTo(x + i, y);
+      let yLine = i % 100 == 0 ? 23 : 10;
+      ctx.lineTo(x + i, y + yLine);
+      ctx.stroke();
+    }
+    ctx.strokeStyle = "black";
   }
 }
 
-function handleMoveRuler(e) {
+function handleRulerMove(e) {
   const { distanceX, distanceY } = distanceToRuler;
 
   if (isDragging.value) {
@@ -108,51 +141,106 @@ function handleMoveRuler(e) {
   }
 }
 
-function checkOnRuler(e) {
-  const { x, y } = rulerPosition;
-  if (e.offsetX >= x && e.offsetX <= x + 400 && e.offsetY >= y && e.offsetY <= y + 70) {
-    distanceToRuler.distanceX = e.offsetX - x;
-    distanceToRuler.distanceY = e.offsetY - y;
-    isDragging.value = true;
-    return true;
-  }
-  return false;
-}
-// function handleRulerRange(e) {
-//   if(e.offsetX >= 250 && e.offsetX <= 650 && e.offsetY >= 250 && e.offsetY <= 470) {
-//     console.log('在尺子周围');
-//   }
-// }
-
 function handleMoveRulerEnd() {
   isDragging.value = false;
+}
+
+// 3.沿着尺子画线模块
+const rulerEquation = reactive({
+  k: 0,
+  b: 0,
+});
+// 判断是否在尺子附近进行吸附
+function handleRulerAround(e) {
+  if (
+    e.offsetX >= rulerPosition.x &&
+    e.offsetX <= rulerPosition.x + 420 &&
+    e.offsetY >= rulerPosition.y - 30 &&
+    e.offsetY <= rulerPosition.y
+  ) {
+    paintType.value = 3;
+  }
+}
+
+// 获取尺子直线方程
+function getLineEquation() {
+  let angle = 0; // 角度值
+  rulerEquation.k = parseFloat(Math.tan((angle * Math.PI) / 180).toFixed(2));
+  rulerEquation.b = parseFloat(
+    (rulerPosition.y - rulerEquation.k * rulerPosition.x).toFixed(2)
+  );
+}
+
+// 获取垂直点
+function countVerticalPoint(x, y) {
+  const { k, b } = rulerEquation;
+
+  const targetPoint = {
+    x: 0,
+    y: 0,
+  };
+  targetPoint.x = (x + k * y - k * b) / (k * k + 1);
+  targetPoint.y = k * x + b - 1;
+
+  return targetPoint;
+}
+
+// 开始画吸附直线
+function handleRulerLineStart(e) {
+  getLineEquation();
+  const targetPoint = countVerticalPoint(e.offsetX, e.offsetY);
+  ctx.beginPath();
+  ctx.moveTo(targetPoint.x, targetPoint.y);
+  flag = true;
+}
+
+// 移动吸附直线
+function handleRulerLineMove(e) {
+  if (flag) {
+    const targetPoint = countVerticalPoint(e.offsetX, e.offsetY);
+    ctx.lineTo(targetPoint.x, targetPoint.y);
+    ctx.stroke();
+  }
+}
+
+// 结束吸附直线
+function handleRulerLineEnd() {
+  ctx.closePath();
+  flag = false;
 }
 
 onMounted(() => {
   initCanvas();
   // 鼠标按下绘图
   canvas.onmousedown = (e) => {
+    if (isShowRuler.value) {
+      checkOnRuler(e) || handleRulerAround(e);
+    }
+
     switch (paintType.value) {
       case 1:
         handlePaintLineStart(e);
         break;
-      case 2:
-        checkOnRuler(e);
+      // case 2:
+      //   checkOnRuler(e);
+      //   break;
+      case 3:
+        handleRulerLineStart(e);
         break;
     }
   };
 
   // 鼠标移动绘图
   canvas.onmousemove = (e) => {
-    // if (paintType.value == 1) {
-    //   handlePaintLineMove(e);
-    // }
     switch (paintType.value) {
       case 1:
         handlePaintLineMove(e);
         break;
       case 2:
-        handleMoveRuler(e);
+        handleRulerMove(e);
+        break;
+      case 3:
+        handleRulerLineMove(e);
         break;
     }
   };
@@ -165,6 +253,9 @@ onMounted(() => {
         break;
       case 2:
         handleMoveRulerEnd();
+        break;
+      case 3:
+        handleRulerLineEnd();
         break;
     }
   };
