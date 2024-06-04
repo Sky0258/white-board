165adaa1d4f08407aaeee232c7b3b9119519a511
skybiubiubiu <1823471207@qq.com>
2024-06-04 09:38:19 +0800

    任意线逻辑抽象出来

--- src/view/WhiteBoard.vue
+++ src/view/WhiteBoard.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <button @click="{paintType = 1;}">画任意线</button>
+    <button @click="{paintType = 2;}">直尺</button><br>
     <canvas ref="canvasRef" width="800" height="600"></canvas>
   </div>
 </template>
@@ -7,49 +9,63 @@
 <script setup>
 import { ref, onMounted } from "vue";
 const canvasRef = ref(null);
+let canvas = null;
+let ctx = null;
+let flag = false;
+let paintType = ref(1);
 
-
-onMounted(() => {
-  const canvas = canvasRef.value;
-  const ctx = canvas.getContext("2d");
-
+function initCanvas() {
+  canvas = canvasRef.value;
+  ctx = canvas.getContext("2d");
   // 设置画板线条的大小，背景色等
   ctx.lineWidth = 2;
   ctx.strokeStyle = "black";
-  let flag = false;
   ctx.fillStyle = "#e0e0e0";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-
   // 平滑所画的曲线
   // // 开启抗锯齿
   // ctx.imageSmoothingEnabled = true;
-  // // 设置线条样式
   // ctx.lineJoin = 'round';
+}
 
+function handlePaintLineStart(e) {
+  flag = true;
+  ctx.beginPath();
+  ctx.moveTo(e.offsetX, e.offsetY);
+}
 
+function handlePaintLineMove(e) {
+  if (flag) {
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+  }
+}
+
+function handlePaintLineEnd() {
+  ctx.closePath();
+  flag = false;
+}
+
+onMounted(() => {
+  initCanvas();
   // 鼠标按下绘图
   canvas.onmousedown = (e) => {
-    flag = true;
-    ctx.beginPath();
-    ctx.moveTo(e.offsetX, e.offsetY);
-  };
-  // 鼠标抬起停止绘图
-  canvas.onmouseup = (e) => {
-    ctx.closePath();
-    // 首尾封闭成图形并填色
-    // ctx.fillStyle = "red";
-    // ctx.fill();
-    ctx.quadraticCurveTo(200, 200, e.offsetX, e.offsetY);
-    flag = false;
+    if (paintType.value == 1) {
+      handlePaintLineStart(e);
+    }
   };
 
   // 鼠标移动绘图
   canvas.onmousemove = (e) => {
-    if (flag) {
-      console.log(e);
-      // ctx.lineTo(e.offsetX, e.offsetY);
-      ctx.quadraticCurveTo(200,200, e.offsetX, e.offsetY);
-      ctx.stroke();
+    if (paintType.value == 1) {
+      handlePaintLineMove(e);
+    }
+  };
+
+  // 鼠标抬起停止绘图
+  canvas.onmouseup = () => {
+    if (paintType.value == 1) {
+      handlePaintLineEnd();
     }
   };
 });
